ff94f775f27d60311a0b7bb9a6e9b60196693f60
Srivathsan27 <88921029+Srivathsan27@users.noreply.github.com>
2022-07-17 13:21:55 +0530

    Added further documentation
    A detailed description of the process of training and fitting the sample data to the model

--- model.py
+++ model.py
@@ -6,14 +6,27 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import pickle
 
+
+# Reading the Dataset
 df = pd.read_csv('sample2.csv')
 X = df.drop(['Segment'], axis=1)
 y = df['Segment'] 
+
+# Standardising the Data
 scalar = StandardScaler()
 scalar = scalar.fit(X)
 X = scalar.transform(X)
+
+# Splitting the data into testing and training datasets
 X_train, X_test, y_train, y_test = train_test_split(X,y, stratify=y, train_size=0.80, random_state=16)
+
+# Configure the KNN model
+
 KNN = KNeighborsClassifier(n_neighbors=50, weights="distance")
+
+# Training the model with the datasets
 KNN.fit(X_train, y_train)
+
+# Exporting the models for later use
 pickle.dump(KNN, open('model.pkl','wb'))
 pickle.dump(scalar, open('scalar.pkl','wb'))
